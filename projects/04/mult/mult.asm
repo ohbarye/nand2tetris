d418390099befe49c6f5e545465bbd0e7c5d1e2e
ohbarye <over.rye@gmail.com>
2020-05-02 11:39:16 +0900

    Solve chapter 4 quizz: multiply

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,32 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@i
+M=1
+@sum
+M=0
+
+(LOOP)
+    @i
+    D=M
+    @1
+    D=D-M // D = i - m
+    @END
+    D;JGT // if i - n > 0 then goto @END 
+    @0
+    D=M // D = n
+    @sum
+    M=D+M // sum = sum + n
+    @i
+    M=M+1 // i = i + 1
+    @LOOP
+    0;JMP
+(END)
+
+@END
+@sum
+D=M
+@2
+M=D
+@200
+0;JMP
