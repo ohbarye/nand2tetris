23e4166d2755b624563dbe701068bc53f228c4a1
ohbarye <over.rye@gmail.com>
2020-05-06 19:32:10 +0900

    Write A-instruction

--- projects/06/assembler/main.ml
+++ projects/06/assembler/main.ml
@@ -1,8 +1,14 @@
-#use "parser.ml"
+open Parser
 
-let assemble filename =
-  let f = open_in filename in
-  let p = Parser.create f in
+let zfill width s =
+  let to_fill = width - (String.length s) in
+  if to_fill <= 0 then s
+  else (String.make to_fill '0') ^ s
+
+let assemble infilename outfilename =
+  let infile = open_in infilename in
+  let outfile = open_out outfilename in
+  let p = Parser.create infile in
 
   while Parser.has_more_commands p do
     match Parser.command_type p with
@@ -11,13 +17,19 @@ let assemble filename =
         print_endline p.current_line;
         Parser.advance p;
       | A_COMMAND ->
-        print_endline "A instruction";
-        print_endline p.current_line;
-        Parser.advance p;    
-  done
+        Batteries.String.tail p.current_line 1
+          |> Batteries.Big_int.of_string
+          |> Batteries.Big_int.to_string_in_binary
+          |> zfill 15
+          |> Printf.fprintf outfile "0%s\n";
+        Parser.advance p;
+  done;
+  close_in infile;
+  close_out outfile;;
 
 let main () =
-  let filename = Sys.argv.(1) in
-  assemble filename;;
+  let infilename = Sys.argv.(1) in
+  let outfilename = Sys.argv.(2) in
+  assemble infilename outfilename;;
 
 main ()
