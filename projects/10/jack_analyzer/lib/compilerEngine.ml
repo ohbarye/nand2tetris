eb49a2734aef216ec8ca54cadf45498d20b19615
ohbarye <over.rye@gmail.com>
2020-05-26 11:33:13 +0900

    chapter 10: compile symbol, identifier

--- projects/10/jack_analyzer/lib/compilerEngine.ml
+++ projects/10/jack_analyzer/lib/compilerEngine.ml
@@ -14,45 +14,44 @@ let write_element_end tag_name outfile depth =
   let indention = Batteries.String.repeat "  " depth in
   Printf.fprintf outfile "%s" (indention ^ "</" ^ tag_name ^ ">\n")
 
-let compile_keyword tokens outfile depth =
-  let current = match tokens with
-      [] -> raise ArgumentError
-    | head :: _ -> head in
-  match current with
-    | "class" ->
-      write_element_start "class" outfile depth;
-      write_element "keyword" "class" outfile (depth + 1);
-      write_element_end "class" outfile depth;
-    | _ ->
-      raise ArgumentError
+let compile_identifier tokens outfile depth =
+  write_element "identifier" (List.hd tokens) outfile depth
 
-(* 
-let compile_identifier engine =
-let compile_symbol engine = *)
+let compile_symbol tokens outfile depth =
+  write_element "symbol" (List.hd tokens) outfile depth
 
-(* 
-let compile_class engine =
-  write_element "class" engine
-  compile_keyword *)
-
-let _compile tokens outfile depth =
+let rec _compile tokens outfile depth =
   match JackTokenizer.token_type tokens with
     | KEYWORD ->
       compile_keyword tokens outfile depth
-    (* | SYMBOL ->
     | IDENTIFIER ->
-    | INT_CONST ->
+      compile_identifier tokens outfile depth
+    | SYMBOL ->
+      compile_symbol tokens outfile depth
+(*    INT_CONST ->
     | STRING_CONST -> *)
     | _ -> raise ArgumentError
+
+and compile_class tokens outfile depth =
+  let (current, rest) = match tokens with
+      [] -> raise ArgumentError
+    | head :: tail -> (head, tail) in
+  write_element_start current outfile depth;
+  write_element "keyword" current outfile (depth + 1);
+  _compile rest outfile (depth + 1);
+  let rest = List.tl rest in
+  _compile rest outfile (depth + 1);
+  write_element_end current outfile depth
+
+and compile_keyword tokens outfile depth =
+  match List.hd tokens with
+    | "class" ->
+      compile_class tokens outfile depth
+    | _ ->
+      raise ArgumentError
 
 let compile filepath =
   let tokens = JackTokenizer.create filepath in
   let outfile = (Batteries.String.rsplit filepath ~by: "." |> fst) ^ ".impl.xml" |> open_out in
-
   _compile tokens outfile 0;
   close_out outfile
-  (* while JackTokenizer.has_more_tokens tokenizer do
-    Printf.fprintf outfile "%s" (let a :: _ = tokenizer.tokens in a);
-    JackTokenizer.advance tokenizer
-  done; *)
-
